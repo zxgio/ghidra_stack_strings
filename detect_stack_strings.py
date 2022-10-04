40a9bb82b76bdab9891b6b433697d4bf09eac4b6
Giovanni Lagorio <giovanni.lagorio@gmail.com>
2022-10-04 16:48:27 +0200

    small fix

--- detect_stack_strings.py
+++ detect_stack_strings.py
@@ -79,7 +79,7 @@ def emulate():
     emu.mem_map(CODE_ADDR, CODE_SIZE, UC_PROT_EXEC)
     emu.mem_write(min_addr, code)
     emu.mem_map(STACK_ADDR, STACK_SIZE, UC_PROT_READ | UC_PROT_WRITE)
-    emu.reg_write(UC_X86_REG_ESP if bits==32 else UC_X86_REG_RSP, STACK_ADDR + STACK_SIZE)
+    emu.reg_write(UC_X86_REG_ESP if bits==32 else UC_X86_REG_RSP, STACK_ADDR + STACK_SIZE // 2 - 8192)
     emu.reg_write(UC_X86_REG_EBP if bits==32 else UC_X86_REG_RBP, STACK_ADDR + STACK_SIZE // 2)
     all_writes = {}
 
